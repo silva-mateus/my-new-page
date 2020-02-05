62b96d161534fed33b4ecb1b985d38c70021c6a3
silva-mateus <46615092+silva-mateus@users.noreply.github.com>
2020-02-05 15:06:20 -0300

    Updates

---
+++ precache-manifest.0ad986668e4975bd9e9e9cab90dba18b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5db62f2c2e7ab7cdb80db7b7fa1b25",
+    "url": "/my-new-page/index.html"
+  },
+  {
+    "revision": "d5b0c6708673167bc78a",
+    "url": "/my-new-page/static/css/main.a0c559b9.chunk.css"
+  },
+  {
+    "revision": "400755f8e453e222550a",
+    "url": "/my-new-page/static/js/2.9742dc05.chunk.js"
+  },
+  {
+    "revision": "1fedcf5c84e2f9b04a7e15ecdecddbbe",
+    "url": "/my-new-page/static/js/2.9742dc05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b0c6708673167bc78a",
+    "url": "/my-new-page/static/js/main.d4357834.chunk.js"
+  },
+  {
+    "revision": "f3eb7b7c6ab673e1ffd4",
+    "url": "/my-new-page/static/js/runtime-main.849e56cc.js"
+  },
+  {
+    "revision": "2f79f1add6cee90b67e007d2f7af34a1",
+    "url": "/my-new-page/static/media/bg-img.2f79f1ad.jpg"
+  },
+  {
+    "revision": "78ea133fd68a4a634f368be669b76396",
+    "url": "/my-new-page/static/media/fylo.78ea133f.png"
+  },
+  {
+    "revision": "be8c8b8ccf57ec535da7711b8e96d2b8",
+    "url": "/my-new-page/static/media/leticiavalentemakeup.be8c8b8c.png"
+  },
+  {
+    "revision": "a393f5f8b4699f77815301e5303ce451",
+    "url": "/my-new-page/static/media/mateus.a393f5f8.png"
+  },
+  {
+    "revision": "27069f8bfff0cbec0f7752a10d1665d8",
+    "url": "/my-new-page/static/media/profile.27069f8b.png"
+  },
+  {
+    "revision": "14fd7b73ae678be2b97e78d2638994e3",
+    "url": "/my-new-page/static/media/tindog.14fd7b73.png"
+  }
+]);
